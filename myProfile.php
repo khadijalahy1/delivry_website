c1a0304a2ac6352aa2ed2e4a4ca0a675427ffa32
khadija AL AHYANE <khadijalahy1@gmail.com>
2021-07-16 13:37:17 +0200

    update info is done

--- myProfile.php
+++ myProfile.php
@@ -1,15 +1,17 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    
+
     //Connect To dB
     try {
         $db = new PDO('mysql:host=127.0.0.1;dbname=delivry_website;charset=utf8', 'root', "root");
@@ -18,21 +20,21 @@
     }
     //get data from user table
 
-    $responseUser=$db->query('SELECT * FROM user WHERE id ='.$_SESSION['userId']); 
-    $userData=$responseUser->fetch();
-    $fName=$userData['fName'];
-    $lName=$userData['lName'];
-    $phone=$userData['phone'];
-    $email=$userData['email'];
-    $password=$userData['password'];
-    $gender=$userData['gender'];
-    $userName=$userData['userName'];
+    $responseUser = $db->query('SELECT * FROM user WHERE id =' . $_SESSION['userId']);
+    $userData = $responseUser->fetch();
+    $fName = $userData['fName'];
+    $lName = $userData['lName'];
+    $phone = $userData['phone'];
+    $email = $userData['email'];
+    $password = $userData['password'];
+    $gender = $userData['gender'];
+    $userName = $userData['userName'];
 
     //get all the addresses of this user and store them in array
 
-    $responseAddress=$db->query('SELECT address FROM address WHERE id_user ='.$_SESSION['userId']); 
- 
-  
+    $responseAddress = $db->query('SELECT address FROM address WHERE id_user =' . $_SESSION['userId']);
+
+
 
 
 
@@ -43,24 +45,79 @@
 
     <form action="updateInfo.php" method="POST">
         <label for="">first name</label>
-        <input type="text" name="fName" value="<?php echo $fName; ?>" ></br>
+        <input type="text" name="fName" value="<?php echo $fName; ?>">
+        <?php
+        if (isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['fNameErr'] != '') {
+            echo '*' . $_SESSION['infoUpdateErr']['fNameErr'];
+        }
+        ?>
+        </br>
         <label for="">last name</label>
-        <input type="text"  name="lName" value="<?php echo $lName; ?>"></br>
+        <input type="text" name="lName" value="<?php echo $lName; ?>">
+        <?php
+        if (isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['lNameErr'] != '') {
+            echo '*' . $_SESSION['infoUpdateErr']['lNameErr'];
+        }
+        ?>
+        </br>
         <label for="">username</label>
-        <input type="text" name="userName" value="<?php echo $userName; ?>"></br>
-        <label for="">phone</label>
-        <input type="text" name="phone" value="<?php echo $phone; ?>"></br>
-        <label for="">email</label>
-        <input type="text" name="email" value="<?php echo $email; ?>"></br>
-        <label for="">password</label>
-        <input type="text" name="password" value="<?php echo $password; ?>"></br>
-        
-        <select name="gender" id="" value="<?php echo $gender; ?>" >
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-        </select></br>
+        <input type="text" name="userName" value="<?php echo $userName; ?>">
+        <?php  
+        if(isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['userNameErr']!='')
+        {
+            echo '*'.$_SESSION['infoUpdateErr']['userNameErr'];
 
-        <input type="submit"  value="update">
+        }
+        ?>
+    </br>
+        <label for="">phone</label>
+        <input type="text" name="phone" value="<?php echo $phone; ?>">
+        <?php  
+        if(isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['phoneErr']!='')
+        {
+            echo '*'.$_SESSION['infoUpdateErr']['phoneErr'];
+
+        }
+        ?>
+    </br>
+        <label for="">email</label>
+        <input type="text" name="email" value="<?php echo $email; ?>">
+        <?php  
+        if(isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['emailErr']!='')
+        {
+            echo '*'.$_SESSION['infoUpdateErr']['emailErr'];
+
+        }
+        ?>
+    </br>
+        <label for="">password</label>
+        <input type="text" name="password" value="<?php echo $password; ?>">
+        <?php  
+        if(isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['passwordErr']!='')
+        {
+            echo '*'.$_SESSION['infoUpdateErr']['passwordErr'];
+
+        }
+        ?>
+    </br>
+
+    <label for="">gender :</label>
+        <select name="gender" id="">
+            <option value="F">Female</option>
+            <option value="M">Male</option>
+        </select><br/>
+       
+    </br>
+
+        <input type="submit" value="update">
+        <?php  
+        if(isset($_SESSION['infoUpdateErr']) && $_SESSION['infoUpdateErr']['msg']!='')
+        {
+            echo $_SESSION['infoUpdateErr']['msg'];
+
+        }
+        ?>
+
 
     </form>
 
@@ -73,24 +130,25 @@
     </form>
 
     </p>
-   
+
 
     <!-- Table: List of available address+delete address-->
     <table>
-        <?php while( $userAddressData=$responseAddress->fetch()){ ?>
+        <?php while ($userAddressData = $responseAddress->fetch()) { ?>
             <tr>
                 <form action="deleteAddress.php" method="Post">
-                <td><?php  echo $userAddressData['address']?></td>
-                <input type="hidden" name="addressValue" value="<?php echo $userAddressData; ?>">
-                <td><input type="submit" value="delete address"></td>
-                
+                    <td><?php echo $userAddressData['address'] ?></td>
+                    <input type="hidden" name="addressValue" value="<?php echo $userAddressData; ?>">
+                    <td><input type="submit" value="delete address"></td>
+
 
                 </form>
-                
+
             </tr>
         <?php } ?>
     </table>
 
-    
+
 </body>
+
 </html>
---
+++ updateInfo.php
@@ -0,0 +1,202 @@
+<?php 
+//update infos
+//create and initialize _SESSION['infoErr'] array_map
+
+session_start();
+$_SESSION['infoUpdateErr'] = array(
+    'fNameErr' => '',
+    'lNameErr' => '',
+    'userNameErr' => '',
+    'passwordErr' => '',
+    'phoneErr' => '',
+    'emailErr' => '',
+    'msg'=>''
+   
+);
+
+//initialize $Error here
+  $Error=array(
+    'fNameErr' => '',
+    'lNameErr' => '',
+    'userNameErr' => '',
+    'passwordErr' => '',
+    'phoneErr' => '',
+    'emailErr' => '',
+    'msg'=>''
+  
+);
+
+
+$updateIsValid=true;
+
+
+
+
+////////////////////////////////////////////utils////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////:  
+
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function checkName($myName)
+{ //fName
+    
+    if (empty($_POST[$myName])) {
+       
+        $GLOBALS['Error'][$myName . 'Err'] = " Name is required";  
+
+    } else {
+        $name = test_input($_POST[$myName]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $GLOBALS['Error'][$myName . 'Err'] = "Only letters and white space allowed";
+        }
+    }
+
+}
+
+function isUsed($myInfo)
+{
+    
+    try {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=delivry_website;charset=utf8', 'root', "root");
+    } catch (Exception $e) {
+        die('Error :' . $e->getMessage());
+    }
+    $response = $db->query('SELECT COUNT(userName) FROM user WHERE ' . $myInfo . '= \'' . $_POST[$myInfo] . '\' AND id != '.$_SESSION['userId']);
+    $data = $response->fetch();
+    if ($data['0'] != 0) {
+        $GLOBALS['Error'][$myInfo . 'Err'] = ' This ' . $myInfo . ' is already used';
+    }
+}
+
+// check registration info and display errors ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+
+
+function checkUpdateInfo()
+{
+  
+
+    //check fName
+
+    checkName('fName');
+    //check lName
+
+    checkName('lName');
+    //check UserName
+
+    checkName('userName');
+   
+    isUsed('userName');
+
+    //check password
+
+    if (!empty($_POST["password"])) {
+        $password = test_input($_POST["password"]);
+        if (strlen($_POST["password"]) <= '8') {
+            $GLOBALS['Error']['passwordErr'] = "Your Password Must Contain At Least 8 Characters!";
+        } elseif (!preg_match("#[0-9]+#", $password)) {
+            $GLOBALS['Error']['passwordErr']  = "Your Password Must Contain At Least 1 Number!";
+        } elseif (!preg_match("#[A-Z]+#", $password)) {
+            $GLOBALS['Error']['passwordErr']  = "Your Password Must Contain At Least 1 Capital Letter!";
+        } elseif (!preg_match("#[a-z]+#", $password)) {
+            $GLOBALS['Error']['passwordErr']  = "Your Password Must Contain At Least 1 Lowercase Letter!";
+        }
+        isUsed('password');
+    } else {
+        $GLOBALS['Error']['passwordErr']  = "password is required   ";
+    }
+
+    //check Email
+
+    if (empty($_POST["email"])) {
+        $GLOBALS['Error']['emailErr'] = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $GLOBALS['Error']['emailErr'] = "Invalid email format";
+        }
+        isUsed('email');
+    }
+
+    //check phone (Only Moroccan numbers are acccepted  06--------/05--------/07--------)
+
+    if (empty($_POST["phone"])){
+
+        $GLOBALS['Error']['phoneErr']  = "phone is required";
+    }
+    else{
+        if(!preg_match("/^[0]{1}[5-7]{1}[0-9]{8}$/", $_POST['phone'])) { 
+            $GLOBALS['Error']["phoneErr"]="Invalid phone number";
+          }
+        else{
+            isUsed('phone');
+        }
+
+    }
+    
+
+
+
+    //check if everything is well
+    if($GLOBALS['Error']!=$_SESSION['infoUpdateErr']){
+       $GLOBALS['updateIsValid']=false;
+
+    }
+
+    //infoErr is ready
+    $_SESSION['infoUpdateErr']= $GLOBALS['Error'];
+    echo '<br/>';
+
+  
+   
+
+
+}
+
+////////////////////////////////////////////////////////////////////////Add new user//////////////////////////////////////////////////////////////////////////////////////
+
+
+function updateUser()
+{
+    try {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=delivry_website;charset=utf8', 'root', "root");
+    } catch (Exception $e) {
+        die('Error :' . $e->getMessage());
+    }
+    //add informations to the User Table
+    $req = $db->prepare('INSERT INTO user(fName,lName,phone,email,password,gender,userName) VALUES(:fName, :lName, :phone, :email, :password, :gender,:userName)');
+    $req = $db->prepare('UPDATE user SET fName=:fName , lName=:lName,userName=:userName,phone=:phone,email=:email,password=:password,gender=:gender WHERE id='.$_SESSION['userId']);
+    $req->execute(array(
+        'fName' => $_POST['fName'],
+        'lName' => $_POST['lName'],
+        'phone' => $_POST['phone'],
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+        'gender' => $_POST['gender'],
+        'userName' => $_POST['userName'],
+
+    ));
+
+
+   
+}
+
+
+////////////////////////////////// Call functions///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+checkUpdateInfo();
+if($updateIsValid){
+    updateUser();
+    $_SESSION['infoUpdateErr']['msg']='The update was done successufully !';
+
+}
+
+header('Location:myProfile.php');
+?>
