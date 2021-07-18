01c0cb2c072af2e72d705839e87830916c8f0154
khadija AL AHYANE <khadijalahy1@gmail.com>
2021-07-18 13:23:37 +0200

    clear errors on click Login

--- backend/navBarFunctions.php
+++ backend/navBarFunctions.php
@@ -5,12 +5,28 @@ if($_GET['do']=='login'){
     echo 'am Logging';
     $_SESSION['showLoggingForm']=true;
     $_SESSION['showSignUpForm']=false;
+    $_SESSION['LoginErr']=array(
+        'userErr'=>'',
+        'passwordErr'=>'',
+        'msg'=>''
     
+    );
+
    
 }
 else if($_GET['do']=='register'){
     $_SESSION['showLoggingForm']=false;
     $_SESSION['showSignUpForm']=true;
+    $_SESSION['infoErr'] = array(
+        'fNameErr' => '',
+        'lNameErr' => '',
+        'userNameErr' => '',
+        'passwordErr' => '',
+        'phoneErr' => '',
+        'emailErr' => '',
+        'addressErr' => '',
+    );
+    
 
 }
 print_r($_SESSION);
