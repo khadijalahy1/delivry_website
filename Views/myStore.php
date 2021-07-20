b894f5ada119d188d4b2f81bbcd28251133c5ad6
khadija AL AHYANE <khadijalahy1@gmail.com>
2021-07-20 02:02:56 +0200

    plusminus picker added

--- Views/myStore.php
+++ Views/myStore.php
@@ -3,6 +3,8 @@
 
 <head>
     <?php include('head.php') ?>
+  
+
 </head>
 
 <body>
@@ -14,41 +16,48 @@
 
     <?php
 
-   
-    
-
-
-    $responseArticle = $db->query('SELECT * FROM article WHERE idStore='.$_GET['storeId']);
-
- 
 
 
 
-  
+
+    $responseArticle = $db->query('SELECT * FROM article WHERE idStore=' . $_GET['storeId']);
+
+
+
+
+
+
 
     ?>
 
     <table>
         <tr>
             <?php
-            while( $myArticles=$responseArticle->fetch()){
+            while ($myArticles = $responseArticle->fetch()) {
 
-                
 
-             ?>
-            
-             <td>
-             <a href="<?php ?>"><img src="<?php echo $myArticles['iconArticle'] ?>" style="width=10% height=10%" alt=""></a></br>
-                 <div><?php echo $myArticles['nameArticle']?> </div>
-                <div><strong><?php echo $myArticles['price'].' DH'?> </strong></div>
-                <div></div>
-                <div><?php echo $myArticles['descriptionArticle']?></div>
-             </td>
+
+            ?>
+
+                <td>
+                    <a href="<?php ?>"><img src="<?php echo $myArticles['iconArticle'] ?>" alt="" style=" display: block;max-width:230px;max-height:95px; width: auto;
+ height: auto;"></a></br>
+  
+                    <div><?php echo $myArticles['nameArticle'] ?> </div>
+                    <div><strong><?php echo $myArticles['price'] ?></strong></div>
+                    <div class="number">
+                        <span class="minus">-</span>
+                        <input type="text" value="1" />
+                        <span class="plus">+</span>
+                    </div>
+                    <div><?php echo $myArticles['descriptionArticle'] ?></div>
+
+                </td>
 
 
             <?php
-             }
-             $responseArticle->closeCursor();
+            }
+            $responseArticle->closeCursor();
             ?>
         </tr>
     </table>
